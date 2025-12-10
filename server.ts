e3597a4eb181fe6891b6ba44a085541ebca30b48
GleanCoder <adityakiranacharya0@gmail.com>
2025-12-11 00:24:56 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,9 @@
-console.log("hello");
+import app from "./src/app.ts";
+
+function startServer() {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`server listening on port: ${port}`);
+  });
+}
+startServer();
